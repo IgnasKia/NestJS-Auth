7a390d2a2e0917b666dcba993c4bf6ca0e8eb774
Ignas Kiaulakys <ignas.kiaulakys@gmail.com>
2021-11-11 13:58:11 +0200

    fix heroku

--- src/chat/gateway/chat.gateway.ts
+++ src/chat/gateway/chat.gateway.ts
@@ -1,7 +1,7 @@
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: { origin: ['http://localhost:8100']}})
+@WebSocketGateway({ cors: { origin: ['https://pokemon-cards-application.herokuapp.com']}})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer()
