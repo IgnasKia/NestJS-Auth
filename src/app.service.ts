a452918a5c2dff3d33bdacf77e58909b7c06ab95
IgnasKia <78438345+IgnasKia@users.noreply.github.com>
2022-02-20 23:29:55 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  const words = ["house", "hello", "world"];
   getHello(): string {
-    return JSON.parse(this.words);
+    return "Hello, what are you doing here?"
   }
 }
